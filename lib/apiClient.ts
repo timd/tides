6b5682ce72103407904f5457a3d685cb593b76f8
Tim Duckett <tim@unea.io>
2022-07-02 17:16:52 +0200

    Adds live data retrieval
    - updates lib/tides to pull live data & parse
    - adds shared ITideObject interface

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -1,26 +1,41 @@
 import axios, { AxiosResponse } from "axios";
 
+export interface ApiData {
+  data: any
+  err: string
+  result: boolean
+}
+
 const endpointUrl = 'https://environment.data.gov.uk/flood-monitoring/id/stations/E73639-anglian/readings.json?today&_sorted'
 
-export async function getApiData() {
+export async function getApiData(): Promise<ApiData> {
 
-  axios.get(endpointUrl).then( response => {
-    return response.data.items;
-  }).catch( err => {
+  let response;
 
+  try {
+    response = await axios.get(endpointUrl);
+  } catch (err) {
     if (axios.isAxiosError(err)) {
       if (err.response && err.response.status === 404) {
        return {
           result: false,
+          data: null,
           err: '404 server error'
         };
       }
     } else {
       return {
         result: false,
+        data: null,
         err: 'Unknown server error'
         }
       };
-    });
-
   };
+
+  return {
+    result: true,
+    data: response?.data.items,
+    err: ""
+  }
+}
+  
--- lib/tides.test.ts
+++ lib/tides.test.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { getApiData } from './apiClient';
+import getTidesData from './tides';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -23,13 +24,13 @@ describe('The API client', () => {
             "@id": "http://environment.data.gov.uk/flood-monitoring/data/readings/E73639-anglian-level-tidal_level-Mean-15_min-mAOD/2022-07-02T11-30-00Z",
             "dateTime": "2022-07-02T11:30:00Z",
             "measure": "http://environment.data.gov.uk/flood-monitoring/id/measures/E73639-anglian-level-tidal_level-Mean-15_min-mAOD",
-            "value": 1.521
+            "value": 2.0
           },
           {
             "@id": "http://environment.data.gov.uk/flood-monitoring/data/readings/E73639-anglian-level-tidal_level-Mean-15_min-mAOD/2022-07-02T11-15-00Z",
             "dateTime": "2022-07-02T11:15:00Z",
             "measure": "http://environment.data.gov.uk/flood-monitoring/id/measures/E73639-anglian-level-tidal_level-Mean-15_min-mAOD",
-            "value": 1.172
+            "value": 1.0
           } 
         ]
       }
@@ -37,8 +38,11 @@ describe('The API client', () => {
 
     mockedAxios.get.mockImplementationOnce(() => Promise.resolve(mockResponse));
 
-    const returnedData = await getApiData();
-    expect(returnedData.data.items).toBeDefined;
+    const data = await getTidesData();
+
+    expect(data.height).toBe(2.0);
+    expect(data.trend).toBe("rising");
+    expect(data.relative).toBe("above");
     await expect(mockedAxios.get).toBeCalledTimes(1);
 
   });
@@ -52,6 +56,19 @@ describe('The API client', () => {
     expect(returnedData).toBeDefined;
     expect(returnedData["result"]).toBeFalsy();
     await expect(mockedAxios.get).toBeCalledTimes(1)
+
+  });
+
+  it('should return error data when the network call fails', async () => {
+
+    const message = "Network Error";
+    mockedAxios.get.mockRejectedValueOnce(new Error(message));
+
+    const data = await getTidesData();
+    expect(data.height).toBe(0);
+    expect(data.trend).toBe("XXX");
+    expect(data.relative).toBe("XXX");
+    await expect(mockedAxios.get).toBeCalledTimes(1);
 
   });
 
