353ea0551ff83a6fddd06e3e34e4dd58f3145d73
Tim Duckett <tim@unea.io>
2022-07-02 14:30:28 +0200

    Adds data API route
    Adds data.ts as API route and dummy data

---
+++ pages/api/data.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  height: number
+  relative: string,
+  trend: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+
+  const data = {
+    height: 1.23,
+    relative: "above",
+    trend : "rising"
+  }
+
+  res.status(200).json(data);  
+    
+}
