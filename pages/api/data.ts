351d38ca94e286df6d88769701082c4e9d6ec12a
Tim Duckett <tim@duckett.de>
2023-12-24 11:07:15 +0000

    Adds render.yaml

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -1,5 +1,5 @@
-import type { NextApiRequest, NextApiResponse } from "next"
 import { ApiData, getApiData } from "../../lib/apiClient"
+import type { NextApiRequest, NextApiResponse } from "next"
 
 export default async function handler(
     req: NextApiRequest,
